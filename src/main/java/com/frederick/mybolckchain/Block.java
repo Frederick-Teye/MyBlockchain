c47499b54efa894cabcbccab063ace4506b0b272
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2025-05-28 22:51:02 +0000

    Create mineBlock() Block to mine the block

--- src/main/java/com/frederick/mybolckchain/Block.java
+++ src/main/java/com/frederick/mybolckchain/Block.java
@@ -11,6 +11,7 @@ public class Block {
     public String previousHash;
     public String data;
     private final long timeStamp;
+    private int nonce;
 
     public Block(String data, String previousHash) {
         this.data = data;
@@ -26,6 +27,15 @@ public class Block {
                 + data
         );
         return calculatedHash;
+    }
+    
+    public void mineBlock(int difficulty) {
+        String target = new String(new char[difficulty]).replace('\0', '0');
+        while (!hash.substring(0, difficulty).equals(target)) {
+            ++nonce;
+            hash = calculateHash();
+        }
+        System.out.println("Block Mined!!! " + hash);
     }
 
 }
--- src/main/java/com/frederick/mybolckchain/Mybolckchain.java
+++ src/main/java/com/frederick/mybolckchain/Mybolckchain.java
@@ -1,8 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.frederick.mybolckchain;
+
+import com.google.gson.GsonBuilder;
+import java.util.ArrayList;
 
 /**
  *
@@ -10,7 +9,46 @@ package com.frederick.mybolckchain;
  */
 public class Mybolckchain {
 
+    public static ArrayList<Block> blockchain = new ArrayList<>();
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        blockchain.add(new Block("First block in blockchain", "0"));
+
+        blockchain.add(new Block("Second block in blockchain",
+                blockchain.get(blockchain.size() - 1).hash));
+
+        blockchain.add(new Block("Third block",
+                blockchain.get(blockchain.size() - 1).hash));
+
+        String blockchainJson = new GsonBuilder().setPrettyPrinting()
+                .create().toJson(blockchain);
+        System.out.println(blockchainJson);
+
     }
+    
+    public static boolean isChainValid() {
+        Block currentBlock;
+        Block previousBlock;
+        
+        for (int i = 1; i < blockchain.size(); i++) {
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i - 1);
+            
+            //compare registered hash and calculated hash
+            if (!currentBlock.hash.equals(currentBlock.calculateHash())) {
+                System.out.println("Curren Hashes are not equal!");
+                return false;
+            }
+            
+            if (!previousBlock.hash.equals(currentBlock.previousHash)) {
+                System.out.println("Previous Hashes are not equal!");
+                return false;
+            } 
+            
+        }
+        
+        return true;
+    }
+
+
 }
