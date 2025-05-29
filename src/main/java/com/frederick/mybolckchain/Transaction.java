b46e8eba3e4c3729dc3e3ac4799896cea4449731
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2025-05-29 23:52:13 +0000

    Create first iteration of Transaction class

---
+++ src/main/java/com/frederick/mybolckchain/Transaction.java
@@ -0,0 +1,44 @@
+package com.frederick.mybolckchain;
+
+import java.security.*;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author frederick
+ */
+public class Transaction {
+
+    public String transactionId; // hash of the transaction
+    public PublicKey sender;
+    public PublicKey reciepient;
+    public float value;
+    public byte[] signature;
+
+    public ArrayList<TrasactionInput> inputs = new ArrayList<>();
+    public ArrayList<TrasactionOutput> outputs = new ArrayList<>();
+
+    private static int sequence = 0; // a rough count of how many transactions have been generated.
+
+    public Transaction(PublicKey from, PublicKey to,
+            float value, ArrayList<TransactionInput> inputs) {
+        this.sender = from;
+        this.reciepient = to;
+        this.value = value;
+        this.inputs = inputs;
+    }
+
+
+    // This calculates the transaction hash which will be used as its ID.
+    private String caluclateHash() {
+        ++sequence; // Increase sequese to avoid same hash for two transactions
+        return StringUtil.applySha256(
+                StringUtil.getStringFromKey(sender)
+                + StringUtil.getStringFromKey(reciepient)
+                + Float.toString(value)
+                + sequence
+        );
+    }
+
+
+}
