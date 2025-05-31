126b4a6b6f0fa164bd0d0ee225fc2da890fef626
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2025-05-31 17:47:14 +0000

    Populate TransactionInput.java

---
+++ src/main/java/com/frederick/mybolckchain/TransactionInput.java
@@ -0,0 +1,15 @@
+package com.frederick.mybolckchain;
+
+/**
+ *
+ * @author frederick
+ */
+public class TransactionInput {
+    public String transactionOutputId;
+    public TransactionOutput UTXO; // Contains the Unspent transaction output
+    
+    public TransactionInput(String transactionOutputId) {
+        this.transactionOutputId = transactionOutputId;
+    }
+    
+}
