95090d5c71e99703bc70be4172c6c43c868a09ba
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2025-05-31 18:02:11 +0000

    Add some comments

--- src/main/java/com/frederick/mybolckchain/TransactionOutput.java
+++ src/main/java/com/frederick/mybolckchain/TransactionOutput.java
@@ -9,9 +9,9 @@ import java.security.PublicKey;
 public class TransactionOutput {
 
     public String id;
-    public PublicKey reciepient;
-    public float value;
-    public String parentTransactionId;
+    public PublicKey reciepient; // New owner of this coins.
+    public float value; // The amount of coins they own
+    public String parentTransactionId;  // The id of transaction this output was created in.
 
     public TransactionOutput(PublicKey reciepient, float value,
             String parentTransactionId) {
